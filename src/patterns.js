c6ca398367f7ad66ca844d19367315a75787235d
Fraddy Oliveira <fraddy.dev@gmail.com>
2021-09-26 01:52:35 +0530

    Add regular expression to match integer.

--- src/patterns.js
+++ src/patterns.js
@@ -3,18 +3,27 @@
  *    @Description: Regexp to check if value is number.
  */
 
-exports.RE_IS_NUMBER = /^([-+]?)0*([0-9][0-9]*)$/;
+const RE_IS_NUMBER = /^([-+]?)0*([0-9][0-9]*)$/;
+
+/*
+ *  @Name : REGEX_INT_NUMBER
+ *  @Description : Regular expression to check if number is integer.
+ */
+
+const REGEX_INT_NUMBER = /^([-+]?)0*([0-9]+)$/;
 
 /**
  *    @Name: RE_IS_ZERO
  *    @Description: Regexp to check if value is zero.
  */
 
-exports.RE_IS_ZERO = /^([-+]?)0+?$/;
+const RE_IS_ZERO = /^([-+]?)0+?$/;
 
 /**
  *    @Name: RE_NON_ZERO
  *    @Description: Regexp to check if value is non-zero.
  */
 
-exports.RE_NON_ZERO = /^([+]?)0*([1-9][0-9]*)$/;
+const RE_NON_ZERO = /^([+]?)0*([1-9][0-9]*)$/;
+
+module.exports = {RE_IS_NUMBER, REGEX_INT_NUMBER, RE_IS_ZERO, RE_NON_ZERO};
