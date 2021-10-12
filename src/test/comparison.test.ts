68dfe7d62c2f954f6633b21265709159746cc84c
Fraddy Oliveira <fraddy.dev@gmail.com>
2021-10-12 23:27:52 +0530

    Code refactor with less than operation test cases.

--- src/test/comparison.test.ts
+++ src/test/comparison.test.ts
@@ -1,8 +1,5 @@
-import chai from 'chai';
-
-const should = chai.should();
-
 import {performance} from 'perf_hooks';
+import {expect} from 'chai';
 
 import {lt} from '../operators/comparison';
 
@@ -10,7 +7,7 @@ let startTime: number = 0;
 
 let debug = false;
 
-describe('less than (lt)', () => {
+describe.only('less than operation', () => {
   beforeEach(() => {
     startTime = performance.now();
   });
@@ -24,62 +21,62 @@ describe('less than (lt)', () => {
   });
 
   describe('both positive number', () => {
-    it('#1', () => {
-      lt('123', '2342').should.be.equal(true);
+    it('123 is less than 2342 is true', () => {
+      expect(lt('123', '2342')).to.be.true;
     });
 
-    it('#2', () => {
-      lt('123213', '2342').should.be.equal(false);
+    it('123213 is less than 2342 is false', () => {
+      expect(lt('123213', '2342')).to.be.false;
     });
   });
 
   describe('both negative number', () => {
-    it('#1', () => {
-      lt('-123213', '-2342').should.be.equal(true);
+    it('-123213 is less than -2342 is true', () => {
+      expect(lt('-123213', '-2342')).to.be.true;
     });
 
-    it('#2', () => {
-      lt('-4564', '-346354535').should.be.equal(false);
+    it('-4564 is less than -346354535 is false', () => {
+      expect(lt('-4564', '-346354535')).to.be.false;
     });
   });
 
   describe('one negative and one positive number', () => {
-    it('#1', () => {
-      lt('-4564', '346354535').should.be.equal(true);
+    it('-4564 is less than 346354535 is true', () => {
+      expect(lt('-4564', '346354535')).to.be.true;
     });
 
-    it('#2', () => {
-      lt('4564', '-346354535').should.be.equal(false);
+    it('4564 is less than -346354535 is false', () => {
+      expect(lt('4564', '-346354535')).to.be.false;
     });
 
-    it('#3', () => {
-      lt('0', '-346354535').should.be.equal(false);
+    it('0 is less than -346354535 is false', () => {
+      expect(lt('0', '-346354535')).to.be.false;
     });
 
-    it('#4', () => {
-      lt('-4534', '-0').should.be.equal(true);
+    it('-4534 is less than -0 is true', () => {
+      expect(lt('-4534', '-0')).to.be.true;
     });
   });
 
   describe('both zeroes number', () => {
-    it('#1', () => {
-      lt('000', '-000').should.be.equal(false);
+    it('000 is less than -000 is false', () => {
+      expect(lt('000', '-000')).to.be.false;
     });
 
-    it('#2', () => {
-      lt('00000', '00000').should.be.equal(false);
+    it('00000 is less than 00000 is false', () => {
+      expect(lt('00000', '00000')).to.be.false;
     });
 
-    it('#3', () => {
-      lt('+0', '-0').should.be.equal(false);
+    it('+0 is less than -0 is false', () => {
+      expect(lt('+0', '-0')).to.be.false;
     });
 
-    it('#4', () => {
-      lt('000', '0000').should.be.equal(false);
+    it('000 is less than 0000 is false', () => {
+      expect(lt('000', '0000')).to.be.false;
     });
 
-    it('#5', () => {
-      lt('0', '0').should.be.equal(false);
+    it('0 is less than 0 is false', () => {
+      expect(lt('0', '0')).to.be.false;
     });
   });
 });
