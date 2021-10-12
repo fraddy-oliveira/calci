256b730e045de1b38d16feee3baefa958b33db9e
Fraddy Oliveira <fraddy.dev@gmail.com>
2021-10-12 23:05:35 +0530

    Code refactor for add test case.

--- src/test/add.test.ts
+++ src/test/add.test.ts
@@ -1,15 +1,23 @@
 import {performance} from 'perf_hooks';
-import chai from 'chai';
 
-const should = chai.should();
+import {expect} from 'chai';
 
-import * as calci from '../calci';
+import {add, addFromArray} from '../calci';
 
 let startTime: number = 0;
 
 let debug = false;
 
-describe('addition (add)', () => {
+const addToVerify = (array: string[]): string => {
+  const sum = array.reduce(
+    (a: string, b: string) => (parseInt(a, 10) + parseInt(b, 10)).toString(),
+    '0',
+  );
+
+  return sum;
+};
+
+describe('Add operation', () => {
   beforeEach(() => {
     startTime = performance.now();
   });
@@ -19,69 +27,67 @@ describe('addition (add)', () => {
       // print seconds required to execute each test case.
       console.log(`timediff: ${performance.now() - startTime}`);
     }
+
     startTime = 0;
   });
 
-  describe('add two numbers', () => {
-    it('#1', () => {
-      calci
-        .add('1123123123123123213123213', '12312312312334233331421322')
-        .should.be.equal('13435435435457356544544535');
+  describe('Add two numbers', () => {
+    it('Sum should be 13435435435457356544544535', () => {
+      const sum = add(
+        '1123123123123123213123213',
+        '12312312312334233331421322',
+      );
+
+      expect(sum).to.equal('13435435435457356544544535');
     });
 
-    it('#2', () => {
-      calci
-        .add('1123123123123123213123213', '12312312312334233331421322')
-        .should.be.equal('13435435435457356544544535');
+    it('Sum should be 13435435435457356544544535', () => {
+      const sum = add(
+        '1123123123123123213123213',
+        '12312312312334233331421322',
+      );
+
+      expect(sum).to.equal('13435435435457356544544535');
     });
 
-    it('#3 - 10 digit', () => {
-      calci.add('1123123423', '8345345523').should.be.equal('9468468946');
+    it('Sum should be 9468468946', () => {
+      const sum = add('1123123423', '8345345523');
+
+      expect(sum).to.equal('9468468946');
     });
 
-    it('#4 - both blank', () => {
-      calci.add(' ', ' ').should.be.equal('0');
+    it('Blank inputs should sum to 0', () => {
+      const sum = add(' ', ' ');
+
+      expect(sum).to.equal('0');
     });
 
-    it('#5 - both zero', () => {
-      calci.add('0', '0').should.be.equal('0');
+    it('Both zero input should sum to 0', () => {
+      const sum = add('0', '0');
+
+      expect(sum).to.equal('0');
     });
   });
 
-  describe('add numbers from array', () => {
-    it('#1', () => {
+  describe('Add random numbers from array', () => {
+    it('Add hundred random numbers', () => {
       const arr = Array.from(
         {length: 100},
         () => parseInt(Math.random() * 1000000000000 + '', 10) + '',
       );
 
-      calci
-        .addFromArray(arr)
-        .should.be.equal(
-          arr
-            .reduce(
-              (a, b) => (parseInt(a, 10) + parseInt(b, 10)).toString(),
-              '0',
-            )
-            .toString(),
-        );
+      expect(addFromArray(arr)).to.equal(addToVerify(arr));
     });
+  });
 
-    it('#2', () => {
+  describe('Add numbers from array', () => {
+    it('Add 3 zeros should sum to 0', () => {
       const arr = ['0', '0', '0'];
-      calci
-        .addFromArray(arr)
-        .should.be.equal(
-          arr
-            .reduce(
-              (a, b) => (parseInt(a, 10) + parseInt(b, 10)).toString(),
-              '0',
-            )
-            .toString(),
-        );
+
+      expect(addFromArray(arr)).to.equal(addToVerify(arr));
     });
 
-    it('#3', () => {
+    it('Sum should be 1243544687030932968237422435890746658808501', () => {
       const arr = [
         '1123123123123123453423213123213',
         '1231231233245123342234523433331421322',
@@ -89,23 +95,16 @@ describe('addition (add)', () => {
         '12312232345453245345312312334233331421322',
         '12312312312334233331421322',
       ];
-      calci
-        .addFromArray(arr)
-        .should.be.equal('1243544687030932968237422435890746658808501');
+
+      expect(addFromArray(arr)).to.equal(
+        '1243544687030932968237422435890746658808501',
+      );
     });
 
-    it('#4', () => {
+    it('Add three similar numbers should sum to 36', () => {
       const arr = ['12', '12', '12'];
-      calci
-        .addFromArray(arr)
-        .should.be.equal(
-          arr
-            .reduce(
-              (a, b) => (parseInt(a, 10) + parseInt(b, 10)).toString(),
-              '0',
-            )
-            .toString(),
-        );
+
+      expect(addFromArray(arr)).to.equal('36');
     });
   });
 });
